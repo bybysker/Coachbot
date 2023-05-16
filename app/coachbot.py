733c61e47aa5be7ba1814d6f774e6cf061a49b82
Elbby AGWU <eagwu@solocal.com>
2023-05-16 11:19:57 +0200

    certif

--- app/coachbot.py
+++ app/coachbot.py
@@ -9,7 +9,7 @@ class CoachBot:
     def __init__(self):
         self.context = []#{'role':'system', 'content': system_prompt}
         openai.api_key = os.getenv("OPENAI_API_KEY")
-        os.environ['REQUESTS_CA_BUNDLE'] = 'caadmin.netskope.com.cer'
+        #os.environ['REQUESTS_CA_BUNDLE'] = 'caadmin.netskope.com.cer'
 
     def get_response(self, user_prompt):
         self.context.append({'role': 'user', 'content': context_prompt + user_prompt})
--- script.sh
+++ script.sh
@@ -1,2 +1,2 @@
 docker build -t coachbot:latest .
-docker run -p 8080:5000 coachbot:latest
+docker run -p 8080:8080 coachbot:latest
