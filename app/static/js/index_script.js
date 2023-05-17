703ae7e46ef1e3c6d694d86d26bfd15f3de45b09
Elbby AGWU <eagwu@solocal.com>
2023-05-17 17:32:59 +0200

    loading stuff

--- app/static/js/index_script.js
+++ app/static/js/index_script.js
@@ -1,3 +1,6 @@
+const loadingIndicator = document.getElementById("loading-indicator");
+const quoteText = document.getElementById("quote-text");
+
 const quotes = [
   "The greatest glory in living lies not in never falling, but in rising every time we fall. - Nelson Mandela",
   "The only way to do great work is to love what you do. - Steve Jobs",
@@ -5,6 +8,7 @@ const quotes = [
   "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
   // Add more quotes here
 ];
+
 
 function displayRandomQuote() {
   const quoteContainer = document.getElementById("quote-container");
@@ -18,18 +22,20 @@ function displayRandomQuote() {
 // Call the function to display a random quote
 displayRandomQuote();
 
-const loadingIndicator = document.getElementById("loading-indicator");
-const quoteText = document.getElementById("quote-text");
+
 
 function startLoadingProcess() {
   // Display the loading indicator
   loadingIndicator.style.display = "block";
 
-  // Update the quote text
-  quoteText.textContent = "Please wait while we process your request...";
+  displayRandomQuote();
 }
 
-// Call the function to start the loading process
-startLoadingProcess();
+const form = document.querySelector('form');
+form.addEventListener('submit', handleSubmit);
 
-  
+// Function to handle form submission
+function handleSubmit(event) {
+  event.preventDefault();
+  startLoadingProcess();
+}
